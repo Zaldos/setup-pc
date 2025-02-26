022df8809eb3fd09f10ae68b0fd78fe1b215c31c
Zaldos <350699+Zaldos@users.noreply.github.com>
2025-02-26 19:08:53 +0000

    Add boostrap

---
+++ Bootstrap.ps1
@@ -0,0 +1,7 @@
+ipconfig /flushdns
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+Invoke-WebRequest https://github.com/Zaldos/setup-pc/archive/refs/heads/main.zip -OutFile "$($env:TEMP)/pcsetup.zip"
+Expand-Archive -LiteralPath "$($env:TEMP)/pcsetup.zip" -DestinationPath "$($env:USERPROFILE)/Downloads" -Force
+Set-Location "$($env:USERPROFILE)/Downloads/setup-pc-main" #<repo-name>-<branchname>
+notepad.exe .\SetupPc.ps1
+"`nOnce you have changed the config run .\SetupPc.ps1 from the terminal`n" }
