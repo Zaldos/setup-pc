3d6e3b21e588a1043943283c712d39654b2d255f
Zaldos <350699+Zaldos@users.noreply.github.com>
2025-02-26 19:11:02 +0000

    Fix boostrap

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -4,4 +4,4 @@ Invoke-WebRequest https://github.com/Zaldos/setup-pc/archive/refs/heads/main.zip
 Expand-Archive -LiteralPath "$($env:TEMP)/pcsetup.zip" -DestinationPath "$($env:USERPROFILE)/Downloads" -Force
 Set-Location "$($env:USERPROFILE)/Downloads/setup-pc-main" #<repo-name>-<branchname>
 notepad.exe .\SetupPc.ps1
-"`nOnce you have changed the config run .\SetupPc.ps1 from the terminal`n" }
+"`nOnce you have changed the config run .\SetupPc.ps1 from the terminal`n"
